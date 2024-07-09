a36a659beb5d3a4f6da5b8f801b789351ab6ed0a
brunoxzr <156040997+brunoxzr@users.noreply.github.com>
2024-07-09 08:40:11 -0300

    Delete Agrinho/project-root directory

--- Agrinho/project-root/Agrinho/project-root/exchange.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const totalRecycled = localStorage.getItem('itemsRecycled') || 0;
-    document.getElementById('totalRecycled').textContent = totalRecycled;
-});
-
-function redeemCoupon() {
-    document.getElementById('couponContainer').style.display = 'block';
-}
